ef1ac81503dbd6362d424b02de7ffb750cd244d7
bwielk <bwielk@gmail.com>
2018-07-14 11:55:49 +0100

    Refactored drivers. All works as it did before

--- public/bundle.js
+++ public/bundle.js
@@ -159,29 +159,22 @@ Driver.prototype.ownedCarsES6 = function(cars){
 
 module.exports = Driver;
 },{}],3:[function(require,module,exports){
-var driver1 = {
-	name: "John",
-	surname: "Smith",
-	drivingLicense: false,
-	drivingLicensePassed: null,
-	yearOfBirth: 1986
-}
+var Driver = require('./driver');
 
-var driver2 = {
-	name: "Sarah",
-	surname: "MacFleetwood",
-	drivingLicense: false,
-	drivingLicensePassed: null,
-	yearOfBirth: 1992
-}
+var driver1 = new Driver("John", "Smith");
+	driver1.drivingLicense = false;
+	driver1.drivingLicensePassed = null;
+	driver1.yearOfBirth = 1986;
 
-var driver3 = {
-	name: "Andrew",
-	surname: "Outkast",
-	drivingLicense: true,
-	drivingLicensePassed: 2001,
-	yearOfBirth: 1980
-}
+var driver2 = new Driver("Sarah", "MacFleetwood");
+	driver2.drivingLicense = false;
+	driver2.drivingLicensePassed = null;
+	driver2.yearOfBirth = 1992;
+
+var driver3 = new Driver("Andrew", "Outkast");
+	driver3.drivingLicense = true;
+	driver3.drivingLicensePassed = 2001;
+	driver3.yearOfBirth = 1980;
 
 module.exports = {driver1, driver2, driver3};
-},{}]},{},[1,2]);
+},{"./driver":2}]},{},[1,2]);
